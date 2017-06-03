cf23096bf157b3ed0f3097fae0c51540fc1df89d
Ram Pai <linuxram@us.ibm.com>
2017-06-02 19:46:15 -0500

    powerpc: track allocation status of all pkeys
    Total 32 keys are available on power7 and above. However
    pkey 0,1 are reserved. So effectively we  have  30 pkeys.
    
    On 4K kernels, we do not  have  5  bits  in  the  PTE to
    represent  all the keys; we only have 3bits.Two of those
    keys are reserved; pkey 0 and pkey 1. So effectively  we
    have 6 pkeys.
    
    This patch keeps track of reserved keys, allocated  keys
    and keys that are currently free.
    
    Also it  adds  skeletal  functions  and macros, that the
    architecture-independent code expects to be available.
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>

--- arch/powerpc/mm/pkeys.c
+++ arch/powerpc/mm/pkeys.c
@@ -16,9 +16,13 @@
 
 bool pkey_inited;
 bool pkey_execute_disable_support;
+int  pkeys_total;		/* total pkeys as per device tree */
+u32  initial_allocation_mask;	/* bits set for reserved keys */
 
 void __init pkey_initialize(void)
 {
+	int os_reserved, i;
+
 	/*
 	 * disable the pkey system till everything
 	 * is in place. A patch further down the
@@ -31,4 +35,28 @@ void __init pkey_initialize(void)
 	 * A patch further down will enable it.
 	 */
 	pkey_execute_disable_support = false;
+
+	/* Lets assume 32 keys */
+	pkeys_total = 32;
+
+#ifdef CONFIG_PPC_4K_PAGES
+	/*
+	 * the OS can manage only 8 pkeys
+	 * due to its inability to represent
+	 * them in the linux 4K-PTE.
+	 */
+	os_reserved = pkeys_total-8;
+#else
+	os_reserved = 0;
+#endif
+	/*
+	 * Bits are in LE format.
+	 * NOTE: 1, 0 are reserved.
+	 * key 0 is the default key, which allows read/write/execute.
+	 * key 1 is recommended not to be used.
+	 * PowerISA(3.0) page 1015, programming note.
+	 */
+	initial_allocation_mask = ~0x0;
+	for (i = 2; i < (pkeys_total - os_reserved); i++)
+		initial_allocation_mask &= ~(0x1<<i);
 }
