5425de19cc3781a805c0c016f2455d74f8bc2715
Ram Pai <linuxram@us.ibm.com>
2017-06-29 10:50:40 -0700

    powerpc: helper function to read,write AMR,IAMR,UAMOR registers
    Implements helper functions to read and write the key related
    registers; AMR, IAMR, UAMOR.
    
    AMR register tracks the read,write permission of a key
    IAMR register tracks the execute permission of a key
    UAMOR register enables and disables a key
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>

--- arch/powerpc/include/asm/book3s/64/pgtable.h
+++ arch/powerpc/include/asm/book3s/64/pgtable.h
@@ -438,6 +438,37 @@ static inline void huge_ptep_set_wrprotect(struct mm_struct *mm,
 		pte_update(mm, addr, ptep, 0, _PAGE_PRIVILEGED, 1);
 }
 
+#include <asm/reg.h>
+static inline u64 read_amr(void)
+{
+	return mfspr(SPRN_AMR);
+}
+static inline void write_amr(u64 value)
+{
+	mtspr(SPRN_AMR, value);
+}
+extern bool pkey_execute_disable_support;
+static inline u64 read_iamr(void)
+{
+	if (pkey_execute_disable_support)
+		return mfspr(SPRN_IAMR);
+	else
+		return 0x0UL;
+}
+static inline void write_iamr(u64 value)
+{
+	if (pkey_execute_disable_support)
+		mtspr(SPRN_IAMR, value);
+}
+static inline u64 read_uamor(void)
+{
+	return mfspr(SPRN_UAMOR);
+}
+static inline void write_uamor(u64 value)
+{
+	mtspr(SPRN_UAMOR, value);
+}
+
 #define __HAVE_ARCH_PTEP_GET_AND_CLEAR
 static inline pte_t ptep_get_and_clear(struct mm_struct *mm,
 				       unsigned long addr, pte_t *ptep)
