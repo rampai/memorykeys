69f3397f51d73e26e7960df40b1ea4c45c10fe85
Ram Pai <linuxram@us.ibm.com>
2017-07-03 15:20:03 -0500

    powerpc: helper to validate key-access permissions of a pte
    helper function that checks if the read/write/execute is allowed
    on the pte.
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>

--- arch/powerpc/include/asm/book3s/64/pgtable.h
+++ arch/powerpc/include/asm/book3s/64/pgtable.h
@@ -494,6 +494,10 @@ static inline void write_uamor(u64 value)
 	mtspr(SPRN_UAMOR, value);
 }
 
+#ifdef CONFIG_PPC_MEM_KEYS
+extern bool arch_pte_access_permitted(u64 pte, bool write, bool execute);
+#endif /* CONFIG_PPC_MEM_KEYS */
+
 #define __HAVE_ARCH_PTEP_GET_AND_CLEAR
 static inline pte_t ptep_get_and_clear(struct mm_struct *mm,
 				       unsigned long addr, pte_t *ptep)
