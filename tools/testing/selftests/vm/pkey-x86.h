3e2451f0cbf81c25253f751998e005856f813c81
Ram Pai <linuxram@us.ibm.com>
2017-07-11 18:53:32 -0700

    selftests/vm: move generic definitions to header file
    Moved all the generic definition and helper functions to the header file.
    
    In preparation for multi-arch support, some definitions were moved to an
    x86-specific header.
    
    cc: Dave Hansen <dave.hansen@intel.com>
    cc: Florian Weimer <fweimer@redhat.com>
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>

---
+++ tools/testing/selftests/vm/pkey-x86.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _PKEYS_X86_H
+#define _PKEYS_X86_H
+
+#ifdef __i386__
+
+#ifndef SYS_mprotect_key
+# define SYS_mprotect_key 380
+#endif
+#ifndef SYS_pkey_alloc
+# define SYS_pkey_alloc	  381
+# define SYS_pkey_free	  382
+#endif
+#define REG_IP_IDX REG_EIP
+#define si_pkey_offset 0x14
+
+#else
+
+#ifndef SYS_mprotect_key
+# define SYS_mprotect_key 329
+#endif
+#ifndef SYS_pkey_alloc
+# define SYS_pkey_alloc	  330
+# define SYS_pkey_free	  331
+#endif
+#define REG_IP_IDX REG_RIP
+#define si_pkey_offset 0x20
+
+#endif
+
+#define PKEY_DISABLE_ACCESS	0x1
+#define PKEY_DISABLE_WRITE	0x2
+#define HPAGE_SIZE		(1UL<<21)
+
+static inline void __page_o_noops(void)
+{
+	/* 8-bytes of instruction * 512 bytes = 1 page */
+	asm(".rept 512 ; nopl 0x7eeeeeee(%eax) ; .endr");
+}
+
+#endif /* _PKEYS_X86_H */
