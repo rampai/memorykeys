0b6770e2f5e7d4a511afc2e11337ad9e5b14bc48
Ram Pai <linuxram@us.ibm.com>
2017-07-13 00:00:28 -0700

    powerpc: ability to create execute-disabled pkeys
    powerpc has hardware support to disable execute on a pkey.
    This patch enables the ability to create execute-disabled
    keys.
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>

--- arch/powerpc/include/uapi/asm/mman.h
+++ arch/powerpc/include/uapi/asm/mman.h
@@ -29,4 +29,10 @@
 #define MAP_STACK	0x20000		/* give out an address that is best suited for process/thread stacks */
 #define MAP_HUGETLB	0x40000		/* create a huge page mapping */
 
+/* override any generic PKEY Permission defines */
+#define PKEY_DISABLE_EXECUTE   0x4
+#undef PKEY_ACCESS_MASK
+#define PKEY_ACCESS_MASK       (PKEY_DISABLE_ACCESS |\
+				PKEY_DISABLE_WRITE  |\
+				PKEY_DISABLE_EXECUTE)
 #endif /* _UAPI_ASM_POWERPC_MMAN_H */
