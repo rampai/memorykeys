371f70f84ab244879189ad75567ab0ec7dbe367a
Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>
2017-09-07 10:40:35 -0500

    mm/mprotect, powerpc/mm/pkeys, x86/mm/pkeys: Add sysfs interface
    Expose useful information for programs using memory protection keys.
    Provide implementation for powerpc and x86.
    
    On a powerpc system with pkeys support, here is what is shown:
    
    $ head /sys/kernel/mm/protection_keys/*
    ==> /sys/kernel/mm/protection_keys/disable_access_supported <==
    true
    
    ==> /sys/kernel/mm/protection_keys/disable_execute_supported <==
    true
    
    ==> /sys/kernel/mm/protection_keys/disable_write_supported <==
    true
    
    ==> /sys/kernel/mm/protection_keys/total_keys <==
    32
    
    ==> /sys/kernel/mm/protection_keys/usable_keys <==
    29
    
    And on an x86 without pkeys support:
    
    $ head /sys/kernel/mm/protection_keys/*
    ==> /sys/kernel/mm/protection_keys/disable_access_supported <==
    false
    
    ==> /sys/kernel/mm/protection_keys/disable_execute_supported <==
    false
    
    ==> /sys/kernel/mm/protection_keys/disable_write_supported <==
    false
    
    ==> /sys/kernel/mm/protection_keys/total_keys <==
    1
    
    ==> /sys/kernel/mm/protection_keys/usable_keys <==
    0
    
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.vnet.ibm.com>

--- arch/x86/include/asm/mmu_context.h
+++ arch/x86/include/asm/mmu_context.h
@@ -129,13 +129,15 @@ static inline void enter_lazy_tlb(struct mm_struct *mm, struct task_struct *tsk)
 		this_cpu_write(cpu_tlbstate.state, TLBSTATE_LAZY);
 }
 
+#define PKEY_INITIAL_ALLOCATION_MAP	1
+
 static inline int init_new_context(struct task_struct *tsk,
 				   struct mm_struct *mm)
 {
 	#ifdef CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS
 	if (cpu_feature_enabled(X86_FEATURE_OSPKE)) {
 		/* pkey 0 is the default and always allocated */
-		mm->context.pkey_allocation_map = 0x1;
+		mm->context.pkey_allocation_map = PKEY_INITIAL_ALLOCATION_MAP;
 		/* -1 means unallocated or invalid */
 		mm->context.execute_only_pkey = -1;
 	}
