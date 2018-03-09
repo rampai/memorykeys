89b3deff1f6febdafe943dd1a4751040c82b174b
Ram Pai <linuxram@us.ibm.com>
2018-03-09 01:20:35 -0600

    powerpc, pkey: make protection key 0 less special
    Applications need the ability to associate an address-range with some
    key and latter revert to its initial default key. Pkey-0 comes close to
    providing this function but falls short, because the current
    implementation disallows applications to explicitly associate pkey-0 to
    the address range.
    
    Lets make pkey-0 less special and treat it almost like any other key.
    Thus it can be explicitly associated with any address range, and can be
    freed. This gives the application more flexibility and power.  The
    ability to free pkey-0 must be used responsibily, since pkey-0 is
    associated with almost all address-range by default.
    
    Even with this change pkey-0 continues to be slightly more special
    from the following point of view.
    (a) it is implicitly allocated.
    (b) it is the default key assigned to any address-range.
    (c) its permissions cannot be modified by userspace.
    
    NOTE: (c) is specific to powerpc only. pkey-0 is associated by default
    with all pages including kernel pages, and pkeys are also active in
    kernel mode. If any permission is denied on pkey-0, the kernel running
    in the context of the application will be unable to operate.
    
    Tested on powerpc.
    
    cc: Thomas Gleixner <tglx@linutronix.de>
    cc: Dave Hansen <dave.hansen@intel.com>
    cc: Michael Ellermen <mpe@ellerman.id.au>
    cc: Ingo Molnar <mingo@kernel.org>
    cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ram Pai <linuxram@us.ibm.com>

--- arch/powerpc/include/asm/pkeys.h
+++ arch/powerpc/include/asm/pkeys.h
@@ -101,10 +101,14 @@ static inline u16 pte_to_pkey_bits(u64 pteflags)
 
 static inline bool mm_pkey_is_allocated(struct mm_struct *mm, int pkey)
 {
-	/* A reserved key is never considered as 'explicitly allocated' */
-	return ((pkey < arch_max_pkey()) &&
-		!__mm_pkey_is_reserved(pkey) &&
-		__mm_pkey_is_allocated(mm, pkey));
+	if (pkey < 0 || pkey >= arch_max_pkey())
+		return false;
+
+	/* Reserved keys are never allocated. */
+	if (__mm_pkey_is_reserved(pkey))
+		return false;
+
+	return __mm_pkey_is_allocated(mm, pkey);
 }
 
 extern void __arch_activate_pkey(int pkey);
@@ -200,6 +204,16 @@ static inline int arch_set_user_pkey_access(struct task_struct *tsk, int pkey,
 {
 	if (static_branch_likely(&pkey_disabled))
 		return -EINVAL;
+
+	/*
+	 * userspace should not change pkey-0 permissions.
+	 * pkey-0 is associated with every page in the kernel.
+	 * If userspace denies any permission on pkey-0, the
+	 * kernel cannot operate.
+	 */
+	if (!pkey)
+		return init_val ? -EINVAL : 0;
+
 	return __arch_set_user_pkey_access(tsk, pkey, init_val);
 }
 
